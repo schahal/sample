69d2872dfa375a1bf70204baace015a5f95bbfc4
schahal <satchahal@gmail.com>
2020-07-10 16:34:40 -0700

    Check version bump on release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", 'r') as f:
 
 setuptools.setup(
     name='samplepkg-schahal',
-    version="0.0.1",
+    version="0.0.2",
     author="Foo Bar",
     author_email="foobar@example.com",
     description="A sample package to help supercharge a number",
